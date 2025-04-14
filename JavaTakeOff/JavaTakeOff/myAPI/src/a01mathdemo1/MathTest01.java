c73e298808772d0256615b325b3a1ba4ecbbf5d4
FlagIX <2689729698@qq.com>
2025-04-14 19:36:56 +0800

    API--Math

---
+++ JavaTakeOff/JavaTakeOff/myAPI/src/a01mathdemo1/MathTest01.java
@@ -0,0 +1,23 @@
+package a01mathdemo1;
+
+public class MathTest01 {
+    public static void main(String[] args) {
+        //求质数
+        System.out.println(isPrime(13));
+        System.out.println(isPrime(10));
+        System.out.println(isPrime(997));
+
+    }
+
+    public static boolean isPrime(int number){
+        int count = 0;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            count++;
+            if(number % i == 0){
+                return false;
+            }
+        }
+        System.out.print(count+" ");
+        return true;
+    }
+}
---
+++ JavaTakeOff/JavaTakeOff/myAPI/src/a01mathdemo1/MathTest02.java
@@ -0,0 +1,20 @@
+package a01mathdemo1;
+
+public class MathTest02 {
+    public static void main(String[] args) {
+        //(课后作业)分别统计有多少个四叶玫瑰数。（答案：3个）
+
+        int count = 0;
+        for (int i = 1000; i < 9999; i++) {
+            int ge = i % 10;
+            int shi = i / 10 % 10;
+            int bai = i / 100 % 10;
+            int qian = i / 1000 % 10;
+            if(i == Math.pow(ge,4)+Math.pow(shi,4)+Math.pow(bai,4)+Math.pow(qian,4)){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+}
