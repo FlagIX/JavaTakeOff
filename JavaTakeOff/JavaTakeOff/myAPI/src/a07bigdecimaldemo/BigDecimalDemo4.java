76f0c1084f35b387fe145c3b4ebbfcd7c47b908b
FlagIX <2689729698@qq.com>
2025-04-19 16:13:50 +0800

    API--BigDecimal

---
+++ JavaTakeOff/JavaTakeOff/myAPI/src/a07bigdecimaldemo/BigDecimalDemo4.java
@@ -0,0 +1,11 @@
+package a07bigdecimaldemo;
+
+import java.math.BigDecimal;
+
+public class BigDecimalDemo4 {
+    public static void main(String[] args) {
+        BigDecimal bd1 = BigDecimal.valueOf(0.266);
+        BigDecimal bd2 = BigDecimal.valueOf(123.266);
+        BigDecimal bd3 = BigDecimal.valueOf(-1.5);
+    }
+}
