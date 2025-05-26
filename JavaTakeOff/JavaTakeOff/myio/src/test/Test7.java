fb595d307d6cfca8994fb2456638763a174fbef8
FlagIX <2689729698@qq.com>
2025-05-26 23:08:46 +0800

    综合练习

---
+++ JavaTakeOff/JavaTakeOff/myio/src/test/Test7.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.io.*;
+
+public class Test7 {
+    public static void main(String[] args) throws IOException {
+        /*
+        实现一个验证程序运行次数的小程序，要求如下：
+        1.当程序运行超过3次时给出提示：本软件只能免费使用3次，欢迎您注册会员后继续使用~
+        2.程序运行演示如下：
+        第一次运行控制台输出：欢迎使用本软件，第1次使用免费~
+        第二次运行控制台输出：欢迎使用本软件，第2次使用免费~
+        第三次运行控制台输出：欢迎使用本软件，第3次使用免费~
+        第四次及之后运行控制台输出：本软件只能免费使用3次，欢迎您注册会员后继续使用~
+        */
+
+        //1.把文件中的数字读取到内存中
+        //原则：
+        //I0:随用随创建
+        //   什么时候不用什么时候关闭
+        BufferedReader br = new BufferedReader(new FileReader("JavaTakeOff\\myio\\count.txt"));
+        String line = br.readLine();
+        br.close();
+
+        int count = Integer.parseInt(line);
+
+        count++;
+
+        //判断
+        if(count <= 3){
+            System.out.println("欢迎使用本软件，第"+count+"次使用免费~");
+        }else {
+            System.out.println("本软件只能免费使用3次，欢迎您注册会员后继续使用~");
+        }
+
+        //将自增的count写出到文件中
+        //创建字符输出流关联文件，如果文件存在则会清空文件
+        BufferedWriter bw = new BufferedWriter(new FileWriter("JavaTakeOff\\myio\\count.txt"));
+        bw.write(count + "");
+        bw.close();
+    }
+}
