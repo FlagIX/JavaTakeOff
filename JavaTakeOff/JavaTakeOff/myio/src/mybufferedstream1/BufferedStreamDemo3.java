0e73167e6fff4b32efcd15b2d18449968ea2d506
FlagIX <2689729698@qq.com>
2025-05-26 14:36:21 +0800

    字符缓冲流

---
+++ JavaTakeOff/JavaTakeOff/myio/src/mybufferedstream1/BufferedStreamDemo3.java
@@ -0,0 +1,34 @@
+package mybufferedstream1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedStreamDemo3 {
+    public static void main(String[] args) throws IOException {
+        /*
+        字符缓冲输入流：
+        构造方法：
+            public BufferedReader(Reader r)
+        特有方法：
+            public String readLine()
+        读一整行
+        */
+
+        //创建对象
+        BufferedReader br = new BufferedReader(new FileReader("JavaTakeOff\\myio\\a.txt"));
+
+        //读取数据
+//        String s = br.readLine();
+//        System.out.println(s);
+
+        //readLine()读完后的返回值是null
+        String line;
+        while ((line = br.readLine()) != null){
+            System.out.println(line);
+        }
+
+        //关流
+        br.close();
+    }
+}
